a962fea16603aa539e5dfd450edd541bf96d706d
Astlvk <astlvk@vip.qq.com>
2018-12-17 16:00:54 +0800

    add element plugin & Text.vue

---
+++ src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>This is title</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  methods: {
+    test () {
+      this.$confirm('test', 'test', {
+        
+      })
+    }
+  }
+}
+</script>
