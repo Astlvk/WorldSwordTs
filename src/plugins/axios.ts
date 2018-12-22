1b66fb05c6a84915a9b84603308cb162984afc09
Astlvk <astlvk@vip.qq.com>
2018-12-22 08:16:47 +0800

    调整代码格式符合tslint

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,18 +1,18 @@
 import Vue, { PluginObject } from 'vue';
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-const config = {
+const config: AxiosRequestConfig = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
 
-const Axios = axios.create(config);
+const Axios: AxiosInstance = axios.create(config);
 
 Axios.interceptors.request.use(
   (cfg) => {
