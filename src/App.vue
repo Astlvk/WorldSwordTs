167b84fe4641e25c9df3bc7d615c7d65713ecd7f
aiwena <astlvk@qq.com>
2018-12-27 12:11:27 +0800

    满足tslint校验

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'App',
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="stylus">
 /* #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
