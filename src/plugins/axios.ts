52396741f520a3e71721cdca8b62a0cf1304124c
aiwena <astlvk@qq.com>
2018-12-28 13:58:38 +0800

    取消重复请求

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,10 +1,36 @@
 import Vue, { PluginObject } from 'vue';
-import axios, { AxiosRequestConfig, AxiosInstance } from 'axios';
+import axios, {
+  AxiosRequestConfig, AxiosInstance, CancelToken,
+  CancelTokenStatic, CancelTokenSource, Canceler, Cancel,
+} from 'axios';
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+// 这里构建用于取消重复请求的代码
+const CancelToken: CancelTokenStatic = axios.CancelToken;
+const pendingList: string[] = [];
+
+const cancelPending = (key: string, cancel: Canceler, pendings: string[]) => {
+  const element = pendings.find((item: string) => {
+    return item === key;
+  });
+  if (element !== undefined) {
+    cancel();
+  } else {
+    pendings.push(key);
+  }
+};
+
+const removePending = (key: string, pendings: string[]) => {
+  const index = pendings.findIndex((item: string) => {
+    return item === key;
+  });
+  if (index !== -1) {
+    pendings.splice(index, 1);
+  }
+};
 
 const config: AxiosRequestConfig = {
   baseURL: 'https://easy-mock.com/mock/5c0e283020e3956ecb888792/',
@@ -18,6 +44,13 @@ const Axios: AxiosInstance = axios.create(config);
 Axios.interceptors.request.use(
   (cfg) => {
     // Do something before request is sent
+    const source: CancelTokenSource = CancelToken.source();
+    cfg.cancelToken = source.token;
+    const baseURL = cfg.baseURL === undefined ? '' : cfg.baseURL;
+    const url = cfg.url === undefined ? '' : cfg.url;
+    const method = cfg.method === undefined ? '' : cfg.method;
+    const key: string = baseURL + url + '&' + method;
+    cancelPending(key, source.cancel, pendingList);
     return cfg;
   },
   (err) => {
@@ -30,6 +63,9 @@ Axios.interceptors.request.use(
 Axios.interceptors.response.use(
   (res) => {
     // Do something with response data
+    const cfg: AxiosRequestConfig = res.config;
+    const key: string = cfg.url + '&' + cfg.method;
+    removePending(key, pendingList);
     return res;
   },
   (err) => {
