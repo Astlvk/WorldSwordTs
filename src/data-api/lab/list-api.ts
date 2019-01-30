9a92d9c39272021c99b92de75516c5e1ea055186
Astlvk <astlvk@vip.qq.com>
2019-01-30 13:33:28 +0800

    添加通过缓存获取数据的getListByCache函数

--- src/data-api/lab/list-api.ts
+++ src/data-api/lab/list-api.ts
@@ -1,6 +1,11 @@
 import request from '../request';
-import { AxiosPromise } from 'axios';
+import { getDataByCache as GetDataByCache } from '@/utils/httpCache';
+import { AxiosPromise, AxiosResponse } from 'axios';
 
 export function getList(query: any): AxiosPromise {
   return request.get('lab/list', { params: query });
+}
+
+export function getListByCache(query: object, callBack: (res: AxiosResponse | undefined) => void) {
+  GetDataByCache('lab/list', query, callBack);
 }
