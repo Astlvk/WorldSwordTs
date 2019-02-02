d36886da35e5290fc650c900a3e872bd07eb9c0c
Astlvk <astlvk@vip.qq.com>
2019-02-02 13:31:28 +0800

    抽取子菜单

---
+++ src/views/lab/IndexAsideMenuItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      <el-submenu index="1-4">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span>选项4</span>
+        </template>
+        <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'IndexAsideMenuItem',
+})
+export default class IndexAsideMenuItem extends Vue {}
+</script>
