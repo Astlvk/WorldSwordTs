98a152b3afe4f9ff9fff161c00b9b97bbb6a0d66
Astlvk <astlvk@vip.qq.com>
2019-02-06 17:33:27 +0800

    菜单测试数据

--- src/data-api/lab/user-api.ts
+++ src/data-api/lab/user-api.ts
@@ -4,8 +4,22 @@ import { AxiosResponse } from 'axios';
 
 export function getMenu(): UserMenu[] {
   const menuArr: UserMenu[] = [
-    new UserMenu('', 'List', '/lab/list', []),
-    new UserMenu('', 'Test', '/lab/test', []),
+    new UserMenu('el-icon-news', 'List', '/lab/list', []),
+    new UserMenu('el-icon-menu', 'Test', '/lab/test', []),
+    new UserMenu('el-icon-location', 'TestChildren', 'TestChildren/lab/ts', [
+      new UserMenu('', '测试子节点1', '/lab/ts1', []),
+      new UserMenu('', '测试子节点2', '测试子节点2/lab/ts2', [
+        new UserMenu('', '测试子节点2-1', '/lab/ts11', []),
+        new UserMenu('', '测试子节点2-2', '/lab/ts22', []),
+      ]),
+    ]),
+    new UserMenu('el-icon-location', 'Hello Wind', 'Hello Wind/lab/tss', [
+      new UserMenu('', '测试子节点3', '/lab/ts1', []),
+      new UserMenu('', '测试子节点4', '测试子节点3/lab/ts2', [
+        new UserMenu('', '测试子节点4-1', '/lab/ts11', []),
+        new UserMenu('', '测试子节点4-2', '/lab/ts22', []),
+      ]),
+    ]),
   ];
   return menuArr;
 }
