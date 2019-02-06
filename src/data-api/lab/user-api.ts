f8a6fb987844536c080ba6e11a84983d7ea0f5df
Astlvk <astlvk@vip.qq.com>
2019-02-06 08:52:17 +0800

    添加user-api.js ---> 包含菜单获取api

--- src/data-api/lab/user-api.ts
+++ src/data-api/lab/user-api.ts
@@ -1,9 +1,19 @@
 import request from '../request';
 import UserMenu from '@/entity/lab/UserMenu';
+import { AxiosResponse } from 'axios';
 
-export function getMenu(token: string): UserMenu[] {
-  const menu: UserMenu[] = [
+export function getMenu(): UserMenu[] {
+  const menuArr: UserMenu[] = [
+    new UserMenu('', 'List', '/lab/list', []),
     new UserMenu('', 'Test', '/lab/test', []),
   ];
-  return menu;
+  return menuArr;
+}
+
+export function getMenuByHttp(): Promise<UserMenu[]> {
+  return new Promise((resolve, reject) => {
+    resolve([
+      new UserMenu('', 'Test', '/lab/test', []),
+    ]);
+  });
 }
