709692147fdeefaf0b498c0ae40815aa9db274e8
Astlvk <astlvk@vip.qq.com>
2019-02-06 10:33:51 +0800

    菜单递归渲染

--- src/views/lab/LabAsideMenuItem.vue
+++ src/views/lab/LabAsideMenuItem.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <el-submenu index="1">
-      <template slot="title">
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      <el-submenu index="1-4">
+    <div v-for="(menu, index) in menus" :key="index">
+      <el-menu-item v-if="menu.children === null || menu.children.length === 0" 
+        :index="menu.path">
+        <i v-if="menu.icon !== null && menu.icon !== ''" 
+          :class="menu.icon"></i>
+        <span>{{ menu.title }}</span>
+      </el-menu-item>
+      <el-submenu v-else :index="menu.path">
         <template slot="title">
-          <i class="el-icon-menu"></i>
-          <span>选项4</span>
+          <i v-if="menu.icon !== null && menu.icon !== ''" 
+            :class="menu.icon"></i>
+          <span>{{ menu.title }}</span>
         </template>
-        <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+        <template v-if="menu.children !== null && menu.children.length > 0">
+          <LabAsideMenuItem :menus="menu.children"></LabAsideMenuItem>
+        </template>
       </el-submenu>
-    </el-submenu>
+    </div>
   </div>
 </template>
 
