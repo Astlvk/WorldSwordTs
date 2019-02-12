58a9a10b6a8825cf49e81597d6f26e80464e9d16
Astlvk <astlvk@vip.qq.com>
2019-02-12 10:02:21 +0800

    调整iocn图标

--- src/data-api/lab/user-api.ts
+++ src/data-api/lab/user-api.ts
@@ -4,16 +4,16 @@ import { AxiosResponse } from 'axios';
 
 export function getMenu(): UserMenu[] {
   const menuArr: UserMenu[] = [
-    new UserMenu('el-icon-news', 'List', '/lab/list', []),
-    new UserMenu('el-icon-menu', 'Test', '/lab/test', []),
-    new UserMenu('el-icon-location', 'TestChildren', 'TestChildren/lab/ts', [
+    new UserMenu('chart', 'List', '/lab/list', []),
+    new UserMenu('bug', 'Test', '/lab/test', []),
+    new UserMenu('edit', 'TestChildren', 'TestChildren/lab/ts', [
       new UserMenu('', '测试子节点1', '/lab/ts1', []),
       new UserMenu('', '测试子节点2', '测试子节点2/lab/ts2', [
         new UserMenu('', '测试子节点2-1', '/lab/ts11', []),
         new UserMenu('', '测试子节点2-2', '/lab/ts22', []),
       ]),
     ]),
-    new UserMenu('el-icon-location', 'Hello Wind', 'Hello Wind/lab/tss', [
+    new UserMenu('list', 'Hello Wind', 'Hello Wind/lab/tss', [
       new UserMenu('', '测试子节点3', '/lab/ts1', []),
       new UserMenu('', '测试子节点4', '测试子节点3/lab/ts2', [
         new UserMenu('', '测试子节点4-1', '/lab/ts11', []),
