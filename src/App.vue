c138d83cd41930cc86d5d55d803f0208bf3b502f
Astlvk <astlvk@vip.qq.com>
2019-02-17 14:58:51 +0800

    去除body元素margin

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@ export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
+body
+  margin: 0px;
 /* #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
