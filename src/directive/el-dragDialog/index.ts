3b46e65eadec4aa32f09bb61ad3736342cf7fbc3
Astlvk <astlvk@vip.qq.com>
2019-02-19 21:36:53 +0800

    拖动指令&拖动指令代码TS化

---
+++ src/directive/el-dragDialog/index.ts
@@ -0,0 +1,14 @@
+import drag from './drag';
+// import { VueConstructor } from 'vue';
+
+// const install = (Vue: VueConstructor) => {
+//   Vue.directive('el-drag-dialog', drag)
+// }
+
+// if (window.hasOwnProperty('Vue')) {
+//   window['el-drag-dialog'] = drag
+//   Vue.use(install); // eslint-disable-line
+// }
+
+// drag.install = install
+export default drag;
