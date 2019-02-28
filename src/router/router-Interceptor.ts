5cc6882cda03a9ec347a6b254f57972cb5eee61c
Astlvk <astlvk@vip.qq.com>
2019-02-28 17:12:19 +0800

    添加路由拦截器

--- src/router/router-Interceptor.ts
+++ src/router/router-Interceptor.ts
@@ -1,11 +1,22 @@
 import Vue from 'vue';
+import store from '@/store';
 import router from './router';
 import { Message } from 'element-ui';
 import { Route, RawLocation } from 'vue-router';
 
+const whiteList: string[] = ['/lab/login', '/home', '/about', '/list'];
+
 router.beforeEach((to: Route, from: Route, next: (to?: RawLocation | false | ((vm: Vue) => any) | void) => void) => {
-  if (to.path) {
-    window.console.log(to.path);
+  const token: string | null = store.state.user.token;
+  // window.console.log(token);
+  if (token) {
+    // window.console.log(to.path);
     next();
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next();
+    } else {
+      next(`/lab/login?redirect=${to.path}`);
+    }
   }
 });
