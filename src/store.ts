e57d58e6f6e856895e61ff82cabe87ff4c0133bd
Astlvk <astlvk@vip.qq.com>
2019-02-28 17:12:43 +0800

    模块引用

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { ModuleTree } from 'vuex';
 import user from './store/modules/user';
+import test from './store/modules/test';
 
 Vue.use(Vuex);
 
+const modules: ModuleTree<any> = {
+  user,
+  test,
+};
+
 export default new Vuex.Store({
-  modules: {
-    user,
-  },
+  modules,
 });
