1b52df21489af0743811386b34feb65e6f3e2edc
Astlvk <astlvk@vip.qq.com>
2019-03-01 15:01:00 +0800

    添加新属性

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,16 +1,22 @@
 import { Store, StoreOptions, Module } from 'vuex';
+import UserMenu from '@/entity/lab/UserMenu';
 
 interface UserStatus {
   token: string | null;
+  userMenu: UserMenu | null;
 }
 
 const user: Module<UserStatus, StoreOptions<any>> = {
   state: {
     token: sessionStorage.getItem('token'),
+    userMenu: null,
   },
   mutations: {
     SET_TOKEN(state: UserStatus, token: string | null): void {
       state.token = token;
+    },
+    SET_USER_MENU(statue: UserStatus, userMenu: UserMenu | null): void {
+      statue.userMenu = userMenu;
     },
   },
   actions: {
