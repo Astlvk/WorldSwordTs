f47a3a954e276b7ef3ededcc749f8fa920efd290
Astlvk <astlvk@vip.qq.com>
2019-03-01 15:00:16 +0800

    添加属性

--- src/entity/lab/UserMenu.ts
+++ src/entity/lab/UserMenu.ts
@@ -1,39 +1,69 @@
 class UserMenu {
-  private icon: string = '';
-  private title: string = '';
-  private path: string = '';
-  private children: UserMenu[] = [];
+  private icon: string;
+  private title: string;
+  private routeName: string;
+  private path: string;
+  private filePath: string;
+  private children: UserMenu[];
 
-  constructor($icon: string , $title: string , $path: string , $children: UserMenu[] ) {
+  constructor(
+    $icon: string, $title: string,
+    $routeName: string, $path: string,
+    $filePath: string, $children: UserMenu[],
+  ) {
     this.icon = $icon;
     this.title = $title;
+    this.routeName = $routeName;
     this.path = $path;
+    this.filePath = $filePath;
     this.children = $children;
   }
 
-  public get getIcon() {
+  public getIcon(): string {
     return this.icon;
   }
-  public get getTitle() {
+
+  public setIcon(icon: string): void {
+    this.icon = icon;
+  }
+
+  public getTitle(): string {
     return this.title;
   }
-  public get getPath() {
+
+  public setTitle(title: string): void {
+    this.title = title;
+  }
+
+  public getRouteName(): string {
+    return this.routeName;
+  }
+
+  public setRouteName(routeName: string): void {
+    this.routeName = routeName;
+  }
+
+  public getPath(): string {
     return this.path;
   }
-  public get getChildren() {
+
+  public setPath(path: string): void {
+    this.path = path;
+  }
+
+  public getFilePath(): string {
+    return this.filePath;
+  }
+
+  public setFilePath(filePath: string): void {
+    this.filePath = filePath;
+  }
+
+  public getChildren(): UserMenu[] {
     return this.children;
   }
 
-  public set setIcon(icon: string) {
-    this.icon = icon;
-  }
-  public set setTitle(title: string) {
-    this.title = title;
-  }
-  public set setPath(path: string) {
-    this.path = path;
-  }
-  public set setChildren(children: UserMenu[]) {
+  public setChildren(children: UserMenu[]): void {
     this.children = children;
   }
 }
