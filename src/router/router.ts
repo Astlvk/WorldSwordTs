f14f7d892d60b06cd70e4696b4ec03f6779d3f47
Astlvk <astlvk@vip.qq.com>
2019-03-09 11:37:15 +0800

    恢复主界面路由

--- src/router/router.ts
+++ src/router/router.ts
@@ -6,32 +6,32 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   redirect: 'home',
-    //   name: 'mainContainer',
-    //   component: mainContainer,
-    //   children: [
-    //     {
-    //       path: 'home',
-    //       name: 'home',
-    //       component: () => import(/* webpackChunkName: "home" */ '@/views/MainContainerHome.vue'),
-    //     },
-    //     {
-    //       path: 'about',
-    //       name: 'about',
-    //       // route level code-splitting
-    //       // this generates a separate chunk (about.[hash].js) for this route
-    //       // which is lazy-loaded when the route is visited.
-    //       component: () => import(/* webpackChunkName: "about" */ '@/views/MainContainerAbout.vue'),
-    //     },
-    //     {
-    //       path: 'list',
-    //       name: 'list',
-    //       component: () => import('@/views/lab/list/List.vue'),
-    //     },
-    //   ],
-    // },
+    {
+      path: '/',
+      redirect: 'home',
+      name: 'mainContainer',
+      component: mainContainer,
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: () => import(/* webpackChunkName: "home" */ '@/views/MainContainerHome.vue'),
+        },
+        {
+          path: 'about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "about" */ '@/views/MainContainerAbout.vue'),
+        },
+        {
+          path: 'list',
+          name: 'list',
+          component: () => import('@/views/lab/list/List.vue'),
+        },
+      ],
+    },
     // Lab下的登陆组件
     {
       path: '/lab/login',
