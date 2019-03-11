5df96fe27fade7e38f772299a44233ad4fae1b71
Astlvk <astlvk@vip.qq.com>
2019-03-11 18:23:56 +0800

    新增退出元素

--- src/views/lab/layout/LabHeader.vue
+++ src/views/lab/layout/LabHeader.vue
@@ -1,8 +1,31 @@
 <template>
-  <div>
-    <h1>
+  <div class="header">
+    <!-- <h1>
       {{ msg }}
-    </h1>
+    </h1> -->
+    <div class="right-menu">
+      <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
+        <div class="avatar-wrapper">
+          <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/40/h/40" class="user-avatar">
+          <i class="el-icon-caret-bottom"/>
+        </div>
+        <el-dropdown-menu slot="dropdown">
+          <router-link to="/">
+            <el-dropdown-item>
+              A
+            </el-dropdown-item>
+          </router-link>
+          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">
+            <el-dropdown-item>
+              B
+            </el-dropdown-item>
+          </a>
+          <el-dropdown-item divided>
+            <span style="display:block;" @click="logout">logout</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
@@ -14,11 +37,75 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 export default class LabHeader extends Vue {
   private msg: string = 'This is Lab root';
+
+  private logout(): void {
+    window.console.log('logout');
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+.header
+  overflow: hidden;
+  padding: 7px 0;
+  border-bottom: 1px solid #d8dce5;
 H1
   text-align center
+a, a:focus, a:hover {
+  cursor: pointer;
+  color: inherit;
+  text-decoration: none;
+}  
+.right-menu {
+  float: right;
+  height: 100%;
+  // line-height: 50px;
+
+  &:focus {
+    outline: none;
+  }
+
+  .right-menu-item {
+    display: inline-block;
+    padding: 0 8px;
+    height: 100%;
+    font-size: 18px;
+    color: #5a5e66;
+    vertical-align: text-bottom;
+    
+    &.hover-effect {
+      cursor: pointer;
+      transition: background .3s;
+
+      &:hover {
+        background: rgba(0, 0, 0, .025)
+      }
+    }
+  }
+
+  .avatar-container {
+    margin-right: 30px;
+
+    .avatar-wrapper {
+      margin-top: 5px;
+      position: relative;
+
+      .user-avatar {
+        cursor: pointer;
+        width: 40px;
+        height: 40px;
+        border-radius: 10px;
+      }
+
+      .el-icon-caret-bottom {
+        cursor: pointer;
+        position: absolute;
+        right: -20px;
+        top: 25px;
+        font-size: 12px;
+      }
+    }
+  }
+}
 </style>
 
