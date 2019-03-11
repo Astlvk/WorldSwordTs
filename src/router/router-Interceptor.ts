a268122d63550e5c06dad494726a2635b288e5bc
Astlvk <astlvk@vip.qq.com>
2019-03-11 13:09:10 +0800

    以类的形式引用对应的拦截器处理逻辑

--- src/router/router-Interceptor.ts
+++ src/router/router-Interceptor.ts
@@ -8,15 +8,16 @@ import 'nprogress/nprogress.css'; // progress bar style
 
 NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
-const whiteList: string[] = ['/lab/login', '/home', '/about'];
-
 router.beforeEach((to: Route, from: Route, next: (to?: RawLocation | false | ((vm: Vue) => any) | void) => void) => {
-  // window.console.log(to.path);
+  window.console.log(to.path);
+  window.console.log(to.path.split('/'));
   const projectPath = to.path.split('/')[1];
-  // window.console.log(projectPath);
+  window.console.log(projectPath);
   switch (projectPath) {
     case 'lab':
-      LabHandler(router, whiteList, to, from, next);
+      const labHandler = new LabHandler();
+      labHandler.handler(router, to, from, next);
+      // LabHandler(router, whiteList, to, from, next);
       break;
     default:
       next();
